014551ffc0995789a440be073fe04c75d278e490
mariusAlc <mannye.skps@gmail.com>
2021-01-19 22:10:04 +0200

    Fixing...

--- distributions/startupway/src/server/index.ts
+++ distributions/startupway/src/server/index.ts
@@ -4,7 +4,7 @@ import "@startupway/database/lib/server";
 import "@startupway/users/lib/server";
 import path from "path";
 import express from "express";
-let server = Server.getInstance();
+const server = Server.getInstance();
 server.registerRouterUI (express.static (path.resolve(__dirname, "../ui")));
 server.start();
 
